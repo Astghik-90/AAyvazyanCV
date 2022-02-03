960f0295d888a7edde04b5ec66ac0e6b1972a84a
Astghik Ayvazyan <77234259+Astghik-90@users.noreply.github.com>
2022-02-03 18:05:58 +0400

    final commit

---
+++ script.js
@@ -0,0 +1,21 @@
+mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
+
+const json = {
+  "email": "astgh1990@gmail.com"
+}
+
+document.getElementById("email").innerText = json.email
